8373fbe0ebcef697a6c4624b941dd452049d922c
cwbychamploo-pixel <YOUR_GITHUB_EMAIL>
2026-08-18 10:52:25 -0400

    first commit

---
+++ First.java/HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * @author Ben Altizer
+ * @version 8/18/2026
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Special Education Teacher");
+        System.out.println("Why: Passionate about helping people");
+        System.out.println("Median salary: $64,270");
+    }
+}

2d90c980a81698d0c675a027dc884c8323c8003e
cwbychamploo-pixel <YOUR_GITHUB_EMAIL>
2026-08-26 09:56:04 -0400

    Add Tests program

---
+++ Tests.java
@@ -0,0 +1,16 @@
+public class Tests
+{
+    public static void main(String[] args)
+    {
+        double score1 = 99.9;
+        double score2 = 38.5;
+        double score3 = 91.4;
+
+        double average = (score1 + score2 + score3) / 3;
+
+        System.out.println("Test score 1: " + score1);
+        System.out.println("Test score 2: " + score2);
+        System.out.println("Test score 3: " + score3);
+        System.out.println("The average of 3 test scores is: " + average);
+    }
+}
